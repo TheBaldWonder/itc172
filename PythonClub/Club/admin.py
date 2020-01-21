5776e10612a8c19b3005ff08e91c69f8c4573ae2
Jonathan <heilman.jonathan83@gmail.com>
2020-01-21 12:33:56 -0800

    Completes Assignment 4

--- PythonClub/Club/admin.py
+++ PythonClub/Club/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Register your models here.
+# This is necessary if they are to appear in the admin app
+
+admin.site.register(Meeting)
+admin.site.register(MeetingMinutes)
+admin.site.register(Resource)
+admin.site.register(Event) 
--- PythonClub/Club/models.py
+++ PythonClub/Club/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField(auto_now=False,auto_now_add=False)
+    meetingtime=models.TimeField(auto_now=False,auto_now_add=False)
+    location=models.TextField()
+    agenda=models.TextField()
+
+    def __str__(self):
+        return self.meetingID
+
+class MeetingMinutes(models.Model):
+    attendance=models.ManyToManyField(User)
+    minutes=models.TextField()
+
+    def __str__(self):
+        return self.meetingID
+
+    class Meta:
+        db_table='meetingminutes'
+
+class Resource(models.Model):
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255)
+    resourceURL=models.URLField(null=True, blank=True)
+    entryDate=models.DateField()
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resourcedescription=models.TextField(null=True, blank=True)
+
+class Event(models.Model):
+    eventtitle=models.CharField(max_length=255)
+    eventlocation=models.TextField()
+    eventdate=models.DateTimeField(auto_now=False,auto_now_add=False)
+    eventtime=models.DateTimeField(auto_now=False,auto_now_add=False)
+    eventdescription=models.TextField(null=True, blank=True)
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self): 
+        return self.eventtitle
+        class Meta:
+            db_table='event'
+            verbose_name_plural='events'    
